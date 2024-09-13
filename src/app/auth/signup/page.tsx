c8af40a3fb8a58ae5c0d25325a0e133b5b19b260
Akshit Gupta <akshu.24gupta@gmail.com>
2024-09-13 19:58:44 +0530

    auth screen fix

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -5,10 +5,10 @@ import { ArrowLeft } from "lucide-react";
 
 const SignupPage = () => {
   return (
-    <div className="w-screen h-screen flex flex-col items-start justify-center gap-10 ">
-      <div className="flex items-center justify-start w-full p-10  ">
+    <div className="w-screen h-screen flex flex-col items-start justify-center gap-2 ">
+      <div className="flex items-center justify-start w-full pt-4 pl-4 ">
         <Link href="/">
-          <Button variant="outline" size="lg">
+          <Button variant="link" size="lg">
             <div className="flex items-center justify-center gap-2">
               <ArrowLeft className="h-5 w-5" />
               <h1 className="">Back</h1>
