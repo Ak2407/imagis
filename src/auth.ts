61a5a8ec444bc9adada4bf5109941f3d3ca0a8cb
Akshit Gupta <akshu.24gupta@gmail.com>
2024-09-13 10:05:24 +0530

    google auth fix

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 
-import Google from "next-auth/providers/Google";
+import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
 import Resend from "next-auth/providers/resend";
 
