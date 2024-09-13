e27ef17c76c48fbd655a90dd379a61c8ea32b70f
Akshit Gupta <akshu.24gupta@gmail.com>
2024-09-13 19:54:17 +0530

    auth back btn fix

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
   return (
     <html lang="en">
       <body>
-        <div className="flex items-center justify-center h-screen w-screen">
+        <div className="flex items-center justify-center h-screen w-screen ">
           {children}
         </div>
       </body>
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -3,20 +3,22 @@ import LoginForm from "./_components/LoginForm";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
-export const runtime = "edge";
-
 const SignupPage = () => {
   return (
-    <div className="relative w-screen h-screen flex items-center justify-center ">
-      <Link href="/" className="absolute top-10 left-10">
-        <Button variant="link" size="lg">
-          <div className="flex items-center justify-center gap-2">
-            <ArrowLeft className="h-5 w-5" />
-            <h1 className="">Back</h1>
-          </div>
-        </Button>
-      </Link>
-      <LoginForm />
+    <div className="w-screen h-screen flex flex-col items-start justify-center gap-10 ">
+      <div className="flex items-center justify-start w-full p-10  ">
+        <Link href="/">
+          <Button variant="outline" size="lg">
+            <div className="flex items-center justify-center gap-2">
+              <ArrowLeft className="h-5 w-5" />
+              <h1 className="">Back</h1>
+            </div>
+          </Button>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center w-full h-full ">
+        <LoginForm />
+      </div>
     </div>
   );
 };
